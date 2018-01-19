69bba7f25f2a9747ab09186d2323f19af83bbc31
Brian <bdburns6389@gmail.com>
2018-01-18 21:48:48 -0600

    Add project files.

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            string Alphabet = "abcdefghijklmnopqrstuvwxyz, .ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            SortedDictionary<char, int> LetterCount = new SortedDictionary<char, int>();
+            foreach (char c in Paragraph)
+            {
+                if (Alphabet.Contains(c))
+                {
+                    if (LetterCount.ContainsKey(c))
+                    {
+                        LetterCount[c] += 1;
+                    }
+                    else
+                    {
+                        LetterCount.Add(c, 1);
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<char,int> c in LetterCount)
+            {
+                Console.WriteLine(String.Format("{0}: {1}", c.Key, c.Value));
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
